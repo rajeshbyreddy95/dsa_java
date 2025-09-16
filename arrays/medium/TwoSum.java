e025542274b99ccc0bfbb0c579a67147816c7417
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-09-16 20:04:26 +0530

    updated code

--- arrays/medium/TwoSum.java
+++ arrays/medium/TwoSum.java
@@ -15,10 +15,23 @@ public class TwoSum {
         }
         return new int[]{};
     }
+
+    public static int[] twoSumV2(int arr[], int n, int target){
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i]+arr[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
     public static void main(String[] args) {
         int arr[] = {2,6,5,8,11};
         int n = arr.length;
         int target = 14;
-        twoSum(arr, n, target);
+        System.out.println(twoSum(arr, n, target));
+        System.out.println(twoSumV2(arr, n, target));
     }
 }
